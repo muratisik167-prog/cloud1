1bf16ff4ee1af368022ea31c86c94317bb5fc40b
muratisik167-prog <muratisik167@gmail.com>
2026-01-03 16:27:54 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask  
 app = Flask(___name___)
 
+"""
+<html>
+        <head>
+            <title>murat işık</title>
+        </head>
+"""
+
 @app.route('/')
 def home():
-  return "istanbul tripten selam"
+  return "merhaba buluttan selam"
